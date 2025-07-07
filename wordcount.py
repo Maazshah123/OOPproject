455e3d8c1e4db22d6951815a45327c4af5f3f3e8
ar162387 <ar162387@gmail.com>
2025-07-07 10:37:14 +0500

    updated code

--- wordcount.py
+++ wordcount.py
@@ -1,13 +1,3 @@
-Paragraph = "hi how are you how old are you how are you doing" 
-count = input("Enter a word to count in the paragraph: ")
-
-wordsToCount= Paragraph.split()   
-#print(wordsToCount[0])
-word_count = 0  
-for word in wordsToCount:
-    if word == count: 
-    
-        word_count = word_count + 1 
-else:
-    print ("the word is not present")
-print(word_count)
+d = { "apples" : 3, "pears" : 4 }
+print(d["apples"]) # the value paired with this key
+print(len(d)) # number of key-value pairs
