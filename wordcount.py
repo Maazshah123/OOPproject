5bc1bb4a030d6cbfc7296c8973fb7c31a920c7db
ar162387 <ar162387@gmail.com>
2025-07-07 12:42:31 +0500

    Updated code

--- wordcount.py
+++ wordcount.py
@@ -1,3 +1,25 @@
-d = { "apples" : 3, "pears" : 4 }
-print(d["apples"]) # the value paired with this key
-print(len(d)) # number of key-value pairs
+Books = {}
+class addBook:
+    def __init__ (self, title):
+     self.title = title 
+
+    def adbook(self):
+        title = input("Enter book title: ")
+        author = input("Enter book author: ")
+        ISBN = int(input("Enter book ISBN: "))
+        genre = input("Enter book genre: ")
+        if title in Books:
+            print("Book already exist")
+            return
+        Books[title] = {
+            "author": author,
+            "ISBN": ISBN,
+            "genre": genre,
+            "is_borrowed": False
+        }
+        print("Book added")
+        return title
+        
+obj = addBook() 
+last_title = obj.adbook()
+print(Books[last_title])
